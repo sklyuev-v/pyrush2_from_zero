f9b6bf66546efdb78f5e956acaed29dc557613a1
Sklyuev Viktor <sklyevv@yandex.ru>
2025-04-29 22:33:40 +0300

    Added gallery

---
+++ static/images/images.js
@@ -0,0 +1,94 @@
+const closeBtn = document.getElementById("close");
+const imagesContainer = document.getElementById("images-placeholder");
+
+function setImages(images) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+
+  images.forEach((image) => {
+    const fullImageName = image;
+    const mainDivEl = document.createElement("div");
+    mainDivEl.className = "col m-2 rounded";
+    mainDivEl.style.width = "310px";
+    mainDivEl.style.height = "200px";
+    imagesContainer.appendChild(mainDivEl);
+
+    const imageDivElement = document.createElement("div");
+    imageDivElement.className = "col m-2 rounded";
+    imageDivElement.style.width = "300px";
+    imageDivElement.style.height = "170px";
+    mainDivEl.appendChild(imageDivElement);
+
+    const imageElement = document.createElement("img");
+    imageElement.src = `/images/${fullImageName}`;
+    imageElement.alt = "Image";
+    imageElement.style.height = "100%";
+    imageElement.style.width = "auto";
+    imageElement.addEventListener("click", () => {
+      modalImg.src = `/images/${fullImageName}`;
+      modal.style.display = "block";
+    });
+
+    imageDivElement.appendChild(imageElement);
+
+    const buttonDivEl = document.createElement("div");
+    buttonDivEl.className = "col m-2 rounded";
+    buttonDivEl.style.width = "310px";
+    buttonDivEl.style.height = "30px";
+    mainDivEl.appendChild(buttonDivEl);
+
+    const btnCopyLink = document.createElement("button");
+    btnCopyLink.textContent = "Скопировать ссылку";
+    btnCopyLink.className = "btn btn-secondary me-2 btn-sm px-2";
+    btnCopyLink.onclick = () => {
+      navigator.clipboard
+        .writeText(window.location.origin + "/images/" + fullImageName)
+        .then(() => {
+          alert("Ссылка скопирована!");
+        })
+        .catch((err) => {
+          alert("Ошибка при копировании ссылки: " + err);
+        });
+    };
+
+    const btnDowload = document.createElement("button");
+    btnDowload.textContent = "Загрузить";
+    btnDowload.className = "btn btn-secondary me-2 btn-sm px-2";
+    btnDowload.onclick = () => {
+      const link = document.createElement("a");
+      link.href = `/images/${fullImageName}`;
+      link.download = fullImageName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
+
+    buttonDivEl.appendChild(btnCopyLink);
+    buttonDivEl.appendChild(btnDowload);
+  });
+}
+
+function loadImages() {
+  fetch("/api/images/")
+    .then((response) => response.json())
+    .then((data) => {
+      imagesContainer.innerHTML = "";
+      if (data.images.length == 0) {
+        const notImagesText = document.createElement("h2");
+        notImagesText.textContent = "Нет загруженных изображений";
+        imagesContainer.appendChild(notImagesText);
+      } else {
+        setImages(data.images);
+      }
+    });
+}
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+loadImages();
